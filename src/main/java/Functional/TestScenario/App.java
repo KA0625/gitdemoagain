7cda0056958ea196827febd98b36f80da7e0d6f8
athirai <mail2aathirai@gmail.com>
2025-11-07 14:14:29 -0600

    added from gitstuff

--- src/main/java/Functional/TestScenario/App.java
+++ src/main/java/Functional/TestScenario/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        system.out.println("after six years demo again ");
+        System.out.println("oh god subconsious mind working");
+        System.out.println("control and spacebar with sysout");
     }
 }
